e5ece184dd62ded778fa95191b3ff0c074e6d3a2
Charlotte Webster <charlotte.shae.webster@gmail.com>
2023-01-24 18:28:07 -0500

    docstrings

--- src/Nav.js
+++ src/Nav.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+/** Renders Nav Bar Component
+ *
+ * App -> Nav
+ */
 function Nav() {
   return (
     <div>
--- src/RoutesList.js
+++ src/RoutesList.js
@@ -5,14 +5,17 @@ import CompaniesPage from './CompaniesPage';
 import JobsPage from './JobsPage';
 import CompanyDetailPage from './CompanyDetailPage';
 
-
+/** App Routes
+ *
+ * App -> RoutesList
+ */
 function RoutesList() {
   return (
     <Routes>
-      <Route path="/" element={ HomePage }/>
-      <Route path="/companies" element={ CompaniesPage }/>
-      <Route path="/jobs" element={ JobsPage }/>
-      <Route path="/companies/:handle" element={ CompanyDetailPage }/>
+      <Route path="/" element={ <HomePage/> }/>
+      <Route path="/companies" element={ <CompaniesPage/> }/>
+      <Route path="/jobs" element={ <JobsPage/> }/>
+      <Route path="/companies/:handle" element={ <CompanyDetailPage/> }/>
       <Route path="*"/>
     </Routes>
   )
